06b9f1fb22f4e39f8a659bea84a744a09e71aff0
해원 <haewon1106@gmail.com>
2024-04-24 12:18:13 +0900

    회원가입 페이지 글 컴포넌트 구현

---
+++ src/commponents/Join/Info.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+function Info(props) {
+  const { text1, text2, color1, color2, fontSize1, fontSize2 } = props;
+
+  const textStyle1 = {
+    color: color1 || 'black',
+    fontSize: fontSize1 || '1.5rem',
+    fontWeight: 'bold',
+  };
+
+  const textStyle2 = {
+    color: color2 || 'black',
+    fontSize: fontSize2 || '1rem',
+  };
+
+  return (
+    <div style={{display:"flex", flexDirection:"column", rowGap: text2 ? "9px" : "0"}}>
+      <span style={textStyle1}>{text1}</span>
+      {text2 && <span style={textStyle2}>{text2}</span>}
+    </div>
+  );
+}
+
+export default Info;
