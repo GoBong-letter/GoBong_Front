7ad5d1b396f5c084faddc23a3f471ff2060c7e3d
해원 <haewon1106@gmail.com>
2024-05-25 22:36:40 +0900

    Feat: 홈화면 구현

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,16 +1,31 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Button from '../commponents/Button';
-import styles from '../styles/Join/Join.module.css';
-import Num from '../commponents/Join/Num';
-import Info from '../commponents/Join/Info';
-import Input from '../commponents/Join/Input';
+import styles from '../styles/Home.module.css'
+import Header from '../commponents/Home/Header';
+import WelcomeMent from '../commponents/Home/WelcomeMent';
+import LetterCount from '../commponents/Home/LetterCount';
+import Nav from '../commponents/Nav';
+import Card from '../commponents/Home/Card';
+import GobongMent from '../commponents/Home/GobongMent';
+import Chart from '../commponents/Home/Chart'
 
 function Home() {
     return (
-       <div>
+       <div className={styles['container']}>
+            <Header />
+            <div className={styles['cardContainer']}>
+                <LetterCount />
+                <WelcomeMent />
+                <Card />
+            </div>
+            <Button text="편지쓰러가기"/>
+            <GobongMent />
+            <Chart />
+            <Nav />
        </div>
     )
 }
 
 export default Home;
+
