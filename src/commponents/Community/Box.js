e2a09f0cdd023de4bc69ea5c6853ba582733cace
해원 <haewon1106@gmail.com>
2024-06-07 11:00:18 +0900

    Feat: 커뮤니티 배경 컴포넌트

---
+++ src/commponents/Community/Box.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Box(){
+    return(
+        <div style={{height:'100vh', backgroundColor:"white", borderRadius:"30px 30px 0 0", overflowY:"scroll"}}></div>
+    )
+}
+export default Box;
