cacfb6abbe29ce40a2d67241ac1c9debe5f9a630
해원 <haewon1106@gmail.com>
2024-06-07 20:10:07 +0900

    Feat: 카테고리 컴포넌트

---
+++ src/commponents/LetterDraw/Category.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from "../../styles/LetterDraw.module.css";
+
+export default function Category(){
+    return(
+        <div className={styles['category']}>
+            <div>능글맞은</div>
+            <div>귀여운</div>
+            <div>빨간색</div>
+            <div>빨간색</div>
+            <div>ISTP</div>
+            <div>SNS</div>
+        </div>
+    )
+}
