fcf56331d1a5213e34b30eb353dbed08c74c8e78
해원 <haewon1106@gmail.com>
2024-06-07 11:01:34 +0900

    Feat: 커뮤니티 유저 정보 컴포넌트

---
+++ src/commponents/Community/CommnuityProfile.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from "../../styles/Community/CommunityProfile.module.css"
+
+function CommunityProfile(){
+    return(
+        <div className={styles['profile-container']}>
+            <img src="/images/cj.png" alt="profile" className={styles['profile-img']}/>
+            <div>
+                <p className={styles['user-name']}>서혀니 바봉</p>
+                <p className={styles["date"]}>2024.03.01</p>
+            </div>
+        </div>
+    )
+}
+export default CommunityProfile;
