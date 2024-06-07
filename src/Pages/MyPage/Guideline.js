8746406bb7c62632e3db397ee994884e3d58725d
해원 <haewon1106@gmail.com>
2024-06-07 20:14:19 +0900

    Feat: 가이드라인 1

---
+++ src/Pages/MyPage/Guideline.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { useNavigate } from 'react-router-dom';
+import GuidelineInfo from "../../commponents/MyPage/Guideline";
+import Text from "../../commponents/MyPage/Text";
+import styles from "../../styles/GuideLine.module.css"
+import Button from "../../commponents/Button";
+import Step from "../../commponents/MyPage/Step";
+
+export default function Guideline(){
+
+  const navigate = useNavigate();
+
+  const handleNext = () => {
+    navigate('/guidelineTwo');
+  };
+
+  const handleHome = () => {
+    navigate('/mypage');
+  };
+    return(
+        <div className={styles['container']}>
+            <Text text1="편지를 쓰고" span="익명의 편지" text3="를 받아봐요!" subtext="랜덤으로 주어지는 5가지 편지중 선택"/>
+            <img src="/images/guide1.png" className={styles['img']}/>
+            <div className={styles['btn-container']}>
+            <Button text="다음" onClick={handleNext}/>
+                <button style={{height:"50px", width:"100%", backgroundColor:"transparent"}} onClick={handleHome}>닫기</button>
+            </div>
+            <Step/>
+        </div>
+    )
+}
