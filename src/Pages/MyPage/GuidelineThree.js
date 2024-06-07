f8b0103893af6c18d416a59fc41f2a0a8e9cb769
해원 <haewon1106@gmail.com>
2024-06-07 20:14:51 +0900

    Feat: 가이드라인 3

---
+++ src/Pages/MyPage/GuidelineThree.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { useNavigate } from 'react-router-dom';
+import GuidelineInfo from "../../commponents/MyPage/Guideline";
+import Text from "../../commponents/MyPage/Text";
+import styles from "../../styles/GuideLine.module.css"
+import Button from "../../commponents/Button";
+import Step from "../../commponents/MyPage/Step";
+
+export default function GuidelineThree(){
+
+  const navigate = useNavigate();
+
+  const handleNext = () => {
+    navigate('/guidelineLast');
+  };
+
+  const handleHome = () => {
+    navigate('/mypage');
+  };
+    return(
+        <div className={styles['container']}>
+            <Text text1="내가 받은 편지에" span="답장" text3="을 보낼 수 있어요!" subtext="답장으로 편지를 읽고 난 나의 마음을 전달해봐요"/>
+            <img src="/images/guide2.png" className={styles['img']}/>
+            <div className={styles['btn-container']}>
+                <Button text="다음" onClick={handleNext}/>
+                <button style={{height:"50px", width:"100%", backgroundColor:"transparent"}} onClick={handleHome}>닫기</button>
+            </div>
+            <Step/>
+        </div>
+    )
+}
