3db678df6c3a1ccf556c3da65732e41c50b1f974
rlagkdms <ithaeun@naver.com>
2024-06-09 22:09:14 +0900

    rename : 컴포넌트 폴더 이름 수정

--- src/Pages/MyPage/AlarmSetting.js
+++ src/Pages/MyPage/AlarmSetting.js
@@ -1,15 +1,25 @@
 import React from "react";
-import Back from "../../commponents/Back";
-import styles from "../../styles/MyPage.module.css"
+import Back from "../../components/Back";
+import styles from "../../styles/MyPage.module.css";
 
-export default function AlarmSetting(){
-    return(
-        <div>
-            <Back text="알림설정"/>
-            <div style={{display:"flex", width:"80vw", justifyContent:"space-between", alignItems:"center", margin:"0 auto", marginTop:"6vw"}}>
-                <p>알림받기</p>
-                <input type="checkbox" id="switch" /><label for="switch">Toggle</label>
-            </div>
-        </div>
-    )
+export default function AlarmSetting() {
+  return (
+    <div>
+      <Back text="알림설정" />
+      <div
+        style={{
+          display: "flex",
+          width: "80vw",
+          justifyContent: "space-between",
+          alignItems: "center",
+          margin: "0 auto",
+          marginTop: "6vw",
+        }}
+      >
+        <p>알림받기</p>
+        <input type="checkbox" id="switch" />
+        <label for="switch">Toggle</label>
+      </div>
+    </div>
+  );
 }
