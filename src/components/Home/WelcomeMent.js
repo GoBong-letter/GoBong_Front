1114fb50a982699e969efa3f91cdcc277258a3ff
해원 <haewon1106@gmail.com>
2024-06-11 00:51:22 +0900

    Docs: 홈화면 멘트 유저 닉네임으로 변경

--- src/components/Home/WelcomeMent.js
+++ src/components/Home/WelcomeMent.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React ,{useContext} from 'react';
 import styles from '../../styles/Home.module.css'
+import { JoinContext } from '../../pages/Join/JoinProvider';
 
-function WelcomeMent() {
+function WelcomeMent({ nickname }) {
     return (
         <div className={styles['mentContainer']}>
-            <h1 className={styles['boldText']}>안녕하세요 <span style={{color:"var(--primary-color)"}}>이해원</span>님 :&#41;</h1>
+            <h1 className={styles['boldText']}>안녕하세요 <span style={{color:"var(--primary-color)"}}>{ nickname }</span>님 :&#41;</h1>
             <p className={styles['subText']}>오늘도 고봉편지에 찾아와주셨군요!</p>
         </div>
     )
