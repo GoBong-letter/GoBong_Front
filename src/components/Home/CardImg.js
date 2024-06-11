84fb631171ea5f1a958c2bd5823d971d89fe1ebb
해원 <haewon1106@gmail.com>
2024-06-11 19:36:09 +0900

    Docs: 카드 컴포넌트 연동

--- src/components/Home/CardImg.js
+++ src/components/Home/CardImg.js
@@ -2,9 +2,26 @@ import React, { useState } from 'react';
 import styles from '../../styles/Card.module.css';
 import Popup from './CardPopup';
 
-function CardImg() {
+function CardImg({ cardImg }) {
     const [isFlipped, setIsFlipped] = useState(false);
     const [isPopupVisible, setPopupVisible] = useState(false);
+
+    const images = [
+        { src: '/images/cards/rice.png', alt: 'rice', text: '쌀밥' },
+        { src: '/images/cards/bean.png', alt: 'bean', text: '콩밥' },
+        { src: '/images/cards/egg_soy.png', alt: 'egg_soy', text: '간장계란밥' },
+        { src: '/images/cards/emelet.png', alt: 'emelet', text: '오므라이스' },
+        { src: '/images/cards/ham_mayo.png', alt: 'ham_mayo', text: '햄마요덮밥' },
+        { src: '/images/cards/salmon.png', alt: 'salmon', text: '연어덮밥' },
+        { src: '/images/cards/bibim.png', alt: 'bibim', text: '비빔밥' }
+    ];
+
+    const filteredImages = images.map(image => {
+        if (cardImg.includes(image.alt)) {
+            return image;
+        }
+        return null; // 일치하지 않는 이미지는 null로 처리
+    }).filter(image => image !== null); // null이 아닌 이미지만 필터링
 
     const handleCardClick = () => {
         setIsFlipped(!isFlipped);
@@ -18,17 +35,18 @@ function CardImg() {
 
     return (
         <>
-            <div className={`${styles.card} ${isFlipped ? styles.flipped : ''}`} onClick={handleCardClick}>
-                <div className={styles["card-inner"]}>
-                    <img src='/images/bab1.png' alt='Card Front' className={styles["card-front"]} />
-                    <div className={styles["card-back"]}>
-                        <div>쌀밥획득!</div>
+            {filteredImages.map((image, index) => (
+                <div key={index} className={`${styles.card} ${isFlipped ? styles.flipped : ''}`} onClick={handleCardClick}>
+                    <div className={styles["card-inner"]}>
+                        <img src={image.src} alt={image.alt} className={styles["card-front"]} />
+                        <div className={styles["card-back"]}>
+                            <div>{image.text} 획득!</div>
+                        </div>
                     </div>
                 </div>
-
-            </div>
+            ))}
             {isPopupVisible && <Popup isVisible={true} onClose={handleClosePopup} />}
-            </>
+        </>
     );
 }
 
