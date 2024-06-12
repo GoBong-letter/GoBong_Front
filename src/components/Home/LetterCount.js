e9973afea049ff5306aeca34099780f96ff78c78
해원 <haewon1106@gmail.com>
2024-06-12 10:48:38 +0900

    Docs: 편지 개수 증가

--- src/components/Home/LetterCount.js
+++ src/components/Home/LetterCount.js
@@ -1,11 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from '../../styles/Home.module.css'
 
 function LetterCount({ LetterCount }) {
+    const [count, setCount] = useState(0);
+
+    useEffect(() => {
+        setCount(0);
+        if (LetterCount > 0) {
+            const interval = setInterval(() => {
+                setCount(prevCount => {
+                    if (prevCount < LetterCount) {
+                        return prevCount + 1;
+                    } else {
+                        clearInterval(interval);
+                        return prevCount;
+                    }
+                });
+            }, 10);
+
+            return () => clearInterval(interval);
+        }
+    }, [LetterCount]);
+
     return (
-        <div className={styles['countBox']}>누적편지 {LetterCount} 💌</div>
+        <div className={styles['countBox']}>누적편지 {count} 💌</div>
     )
 }
 
 export default LetterCount;
-
